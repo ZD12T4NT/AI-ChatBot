ff96e837a2dfd64383d4b575e5c1567702dd8dc3
Zachary Distant <zacdistant@gmail.com>
2025-03-05 14:22:30 +0000

    First commit, files and dependencies added

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+// pages/index.js
+import ChatBox from "../components/ChatBox";
+
+export default function Home() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <ChatBox />
+        </div>
+    );
+}
